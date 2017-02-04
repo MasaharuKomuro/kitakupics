bece811e04d458c479ed2ff7dddd5e06e6e29e51
Masaharu Komuro <komuro-m1@asahi.com>
2017-02-05 05:43:07 +0900

    index.phpを修正

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
- echo "test";
+$channel_access_token = "	
+Long term token which is displayed permanently.7/ubzHaveXYa4q+Q2TwlwrnmMbTt0FUSWWOV5jctpqIgwaSgUTytg3MrRVsoF+c3lq9EDr0T0ggOK6DsK/0vxRq+5o3rnW/6p14RTMabnfC3LeuMeKzNDeT5DbDKGit960o/0L4nc/B4OE0W/euzJAdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "90a98f5c73da5c01de4c982eca67c7f6";
+
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient( $channel access token );
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret ]);
+ 
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('{replyToken}', $textMessageBuilder);
+ 
+echo $response->getHTTPStatus() . ' ' . $response->getBody();
