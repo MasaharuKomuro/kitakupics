36f8fec3b16c70933c568f8985a69e20bb85c422
Masaharu Komuro <komuro-m1@asahi.com>
2017-02-05 09:13:48 +0900

    hdsf

--- send_message.php
+++ send_message.php
@@ -17,7 +17,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient( $channel_access_token
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret ]);
 
 $path = "http://dogbag.info/img/tar_002.jpg";
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder( $path, $path );
-$response = $bot->pushMessage($userId, $textMessageBuilder);
+$imageMessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder( $path, $path );
+$response = $bot->pushMessage($userId, $imageMessageBuilder );
 
 echo $response->getHTTPStatus() . ' ' . $response->getBody();
